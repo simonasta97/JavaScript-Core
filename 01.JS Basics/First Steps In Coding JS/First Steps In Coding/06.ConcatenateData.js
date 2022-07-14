d7a6511b0241316857f0c7502bf50703457053c2
Simona Milcheva <103176056+simonasta97@users.noreply.github.com>
2022-07-14 23:43:36 +0300

    Add files via upload

---
+++ 01.JS Basics/First Steps In Coding JS/First Steps In Coding/06.ConcatenateData.js
@@ -0,0 +1,8 @@
+function concatenateData(fname, lname, age, town){
+    let text = `You are ${fname} ${lname}, a ${age}-years old person from ${town}.`;
+
+    console.log(text);
+}
+
+concatenateData("Maria", "Ivanova", 20, "Sofia");
+concatenateData("Sofia", "Ivanova", 36, "Varna");
